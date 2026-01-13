c253a6e9d3b66547463023e7deea908bf159ac38
mercy <mihretyirga7@gmail.com>
2026-01-13 12:28:27 +0300

    ad connect

---
+++ server/db/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { drizzle } = require('drizzle-orm/mysql2');
+const mysql = require('mysql2/promise');
+const schema = require('./schema');
+
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const db = drizzle(connection, { schema });
+
+module.exports = { db };
