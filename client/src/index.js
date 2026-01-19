a725ed61f7734b064069a81e5a831baccd2d2c35
mercy <mihretyirga7@gmail.com>
2026-01-20 00:06:33 +0300

    fix process is not found error

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,17 @@
 import { Buffer } from 'buffer';
+import process from 'process';
 
 window.global = window;
 window.Buffer = Buffer;
-window.process = require('process');
-window.process = {
-  env: { DEBUG: undefined },
-  version: '',
-  nextTick: (fn) => setTimeout(fn, 0),
-  listeners: () => [],
-  on: () => {},
-  removeListener: () => {}
-};
+window.process = process; // Use the actual process polyfill
+// Add specific overrides for the browser
+window.process.nextTick = (fn) => setTimeout(fn, 0);
+window.process.env = { DEBUG: undefined };
 
-// Use require here to ensure process is defined before App components load simple-peer
 const React = require('react');
 const ReactDOM = require('react-dom/client');
 require('./index.css');
 const App = require('./App').default;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+root.render(<App />);
