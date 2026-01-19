c09f98953a1e159a755966bb46652faaa6c8a19c
mercy <mihretyirga7@gmail.com>
2026-01-19 21:50:02 +0300

    fix process is not found error

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
+import { Buffer } from 'buffer';
+
+// 1. Define Polyfills FIRST
+window.global = window;
+window.Buffer = Buffer;
+window.process = {
+  env: { DEBUG: undefined },
+  version: '',
+  nextTick: (fn) => setTimeout(fn, 0),
+};
+
+// 2. NOW import React and your App
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +21,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
